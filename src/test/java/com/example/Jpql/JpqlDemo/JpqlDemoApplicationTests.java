c748199a64a50a733d79765426afef089b05d814
Akanksha Kumari <53135006+akankshakumari02@users.noreply.github.com>
2021-03-15 11:54:19 +0530

    DONE

---
+++ src/test/java/com/example/Jpql/JpqlDemo/JpqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jpql.JpqlDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
